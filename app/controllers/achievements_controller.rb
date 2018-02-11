fcec16c485798217631ec4e03ccbfbb8f099e60e
Ilyeo <jrag.tkd@gmail.com>
2018-02-11 16:54:19 -0600

    Add specs for achievements controller

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -1,4 +1,8 @@
 class AchievementsController < ApplicationController
+  def index
+    @achievements = Achievement.public_access # or Achievement.where(privacy: :public_access)
+  end
+
   def new
     @achievement = Achievement.new
   end
@@ -6,15 +10,18 @@ class AchievementsController < ApplicationController
   def create
     @achievement = Achievement.new(achievement_params)
     if @achievement.save
-      redirect_to root_path, notice: 'Achievement has been created'
+      redirect_to achievement_path(@achievement), notice: 'Achievement has been created'
     else
       render :new
     end
   end
 
+  def edit
+    @achievement = Achievement.find(params[:id])
+  end
+
   def show
     @achievement = Achievement.find(params[:id])
-    @description = Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(@achievement.description)
   end
 
   private

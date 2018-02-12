b20acd8c9de71689673415736469ebce05a00519
Ilyeo <jrag.tkd@gmail.com>
2018-02-11 21:55:44 -0600

    Add factories for users

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'devise'
 
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -54,4 +55,5 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include Devise::TestHelpers, type: :controller
 end

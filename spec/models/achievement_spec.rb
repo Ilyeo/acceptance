f05d085e2dbd3e3a1d1a602ce33daa195f5473fc
Ilyeo <jrag.tkd@gmail.com>
2018-03-13 11:28:48 -0600

    Add tests to model achievement

--- spec/models/achievement_spec.rb
+++ spec/models/achievement_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe Achievement, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'validations' do
+    it "require title" do
+      achievement = Achievement.new(title: '')
+      # achievement.valid?
+      # expect(achievement.errors[:title]).to include("can't be blank")
+      # expect(achievement.errors[:title]).not_to be_empty
+      expect(achievement.valid?).to be_falsy
+    end
+
+    it 'requires title to be unique for one user' do
+      user = FactoryBot.create(:user)
+      first_achievement = FactoryBot.create(:public_achievement, title: 'Taekwondo', user: user)
+      new_achievement = Achievement.new(title: 'Taekwondo', user: user)
+      expect(new_achievement.valid?).to be_falsy
+    end
+
+    it 'allows different users to have achievements with identical titles' do
+      user1 = FactoryBot.create(:user)
+      user2 = FactoryBot.create(:user)
+      first_achievement = FactoryBot.create(:public_achievement, title: 'Taekwondo', user: user1)
+      new_achievement = Achievement.new(title: 'Taekwondo', user: user2)
+      expect(new_achievement.valid?).to be_truthy
+    end
+
+    it 'belongs to user' do
+      achievement = Achievement.new(title: 'Some title', user: nil)
+      expect(achievement.valid?).to be_falsy
+    end
+
+    it 'has belongs_to user association' do
+      # 1 approach
+      user = FactoryBot.create(:user)
+      achievement = FactoryBot.create(:public_achievement, user: user)
+      expect(achievement.user).to eq(user)
+
+      # 2 approach
+      u = Achievement.reflect_on_association(:user)
+      expect(u.macro).to eq(:belongs_to)
+    end
+  end
+
 end
